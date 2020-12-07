900cbc1bf849cc976d3c6ded083eb91e5d7ae055
Sofiag8 <1943@holbertonschool.com>
2020-12-07 23:10:53 +0000

    Create a file using puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# creating a file with puppet in tmp
+
+file { '/tmp/holberton':
+  ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
