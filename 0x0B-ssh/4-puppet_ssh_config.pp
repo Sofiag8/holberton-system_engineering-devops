3b7b70f41c5028af1e57dfcd5c6dd26b58ba164e
Sofiag8 <1943@holbertonschool.com>
2020-12-09 01:46:33 +0000

    :

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # connecting to a server without using password
-file_line { '/etc/ssh/ssh_config',
+file_line { '/etc/ssh/ssh_config':,
      ensure => 'present'
      path   => '/etc/ssh/ssh_config',
      line   => 'PasswordAuthentication no; IdentifyFile ~/.ssh/holberton',
