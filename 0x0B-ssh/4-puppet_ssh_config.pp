7096bbd5a5d0af7a0d518a29bb13034a0af8acfc
Sofiag8 <1943@holbertonschool.com>
2020-12-09 02:26:17 +0000

    skfsd

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,13 +1,6 @@
 # connecting to a server without
-# using password
-file_line {' password off':
-     ensure => 'present',
-     path   => '/etc/ssh/ssh_config',
-     line   => 'PasswordAuthentication no',
-     match  => 'PasswordAuthentication yes',
-}
-file_line { 'server connection':,
-     ensure => 'present'
-     path   => '/etc/ssh/ssh_config',
-     line   => 'IdentifyFile ~/.ssh/holberton',
+file_line { '/etc/ssh/ssh_config':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no;    IdentityFile ~/.ssh/holberton',
 }
