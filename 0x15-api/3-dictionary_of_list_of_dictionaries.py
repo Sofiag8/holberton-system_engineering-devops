e09c1edc1d82e6cbd1d9b5853e361dc87473e19e
Sofiag8 <1943@holbertonschool.com>
2021-02-02 03:06:11 +0000

    exporting python script data to json file, a dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+"""
+import json
+import requests
+
+
+if __name__ == "__main__":
+    jsonFile = 'todo_all_employees.json'
+    url_user = 'https://jsonplaceholder.typicode.com/users'
+    url_todo = 'https://jsonplaceholder.typicode.com/todos'
+    user_req = requests.get(url_user).json()
+    todo_req = requests.get(url_todo).json()
+    json_format = {}
+    info_user_list = []
+    for all_users in user_req:
+        for all_tasks in todo_req:
+            if all_users['id'] == all_tasks['userId']:
+                user_infor_dict = {
+                    "username": all_users['username'],
+                    "task": all_tasks['title'],
+                    "completed": all_tasks['completed'],
+                }
+                info_user_list.append(user_infor_dict)
+        json_format[all_users['id']] = info_user_list
+    with open(jsonFile, "w") as file:
+        json.dump(json_format, file)
