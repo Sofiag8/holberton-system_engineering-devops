7a6e64b6736328b6cf8642e43310b633f907c8bd
Sofiag8 <1943@holbertonschool.com>
2021-02-02 02:02:43 +0000

    exporting python script data to a json format

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Using previos task code, export data in the CSV format
+
+Requirements:
+    Records all tasks that are owned by the employee
+    Specific format "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+    Exported file name must be <user_id>.csv
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url_user = 'https://jsonplaceholder.typicode.com/users/{}'\
+        .format(user_id)
+    user_req = requests.get(url_user).json()
+    userId_parameter = '?userId={}'.format(user_id)
+    url_todo = 'https://jsonplaceholder.typicode.com/todos{}'\
+        .format(userId_parameter)
+    todo_req = requests.get(url_todo).json()
+    todo_total = len(todo_req)
+
+    info_list = []
+    for all_tasks in todo_req:
+        tasks_infor_dict = {
+                "task": all_tasks['title'],
+                "completed": all_tasks['completed'],
+                "username": user_req['username'],
+        }
+        info_list.append(tasks_infor_dict)
+    json_format = {user_id: info_list}
+
+    jsonFile = "{}.json".format(user_id)
+    with open(jsonFile, "w") as file:
+        json.dump(json_format, file)
