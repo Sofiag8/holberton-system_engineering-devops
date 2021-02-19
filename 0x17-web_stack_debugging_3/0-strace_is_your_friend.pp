730a19fb2596de0776f9742dca6e0c00ebb63933
Sofiag8 <1943@holbertonschool.com>
2021-02-19 00:48:01 +0000

    mm

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fixing error on wp-setting for an extra p on  php extension
-exec { 'automate php file error':
+exec { 'automate-php-file-error':
      command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-     path => '/bin/',
+     path    => '/bin/',
 }
