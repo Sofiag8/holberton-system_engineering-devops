5bdd76824bf1e7e2713d18045e0785921345d43e
Sofiag8 <1943@holbertonschool.com>
2021-02-25 22:15:07 +0000

    requirement

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,4 @@
-# Sky is the limit, let's bring that limit higher
- 
+# Sky is the limit, let's bring that limit higher 
 exec { 'fixing':
   path     => '/usr/bin:/usr/sbin:/bin',
   provider => shell,
